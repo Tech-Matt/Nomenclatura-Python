eeba58787d2a83cd68b78c07049df182ca65ef0f
Gabriele Fordini <48223879+ColdWaters@users.noreply.github.com>
2020-02-07 18:02:02 +0100

    Aggiornamento variabile principale
    la variabile 'formula' è stata cambiata in 'ins_form'

--- nomenclatura.py
+++ nomenclatura.py
@@ -25,8 +25,8 @@ with open("elementi.txt", "r") as nomi_el:
         #Inserisco la coppia elemento-nome nel dizionario
         dic[coppia[0]] = coppia[1]
 
-#Formula chimica nel formato "PCl3" (elemento-indice)
-formula = input("Inserisci la formula chimica: ")
+#formula chimica nel formato "PCl3" (elemento-indice)
+ins_form = input("Inserisci la formula chimica: ")
 
 #Lista che contiene gli elementi scritti nella formula
 elementi = []
@@ -35,28 +35,28 @@ elementi = []
 indici = []
 
 #Leggo ogni lettera in formula
-for i in range(len(formula)):
-    if formula[i].isupper() == True: #Controllo se la lettera e' maiuscola o minuscola per memorizzare l'elemento
-        if (len(formula) - 1) == i: #Se mi trovo a fine riga
-            elementi.append(formula[i]) #Inserisco l'elemento nella lista per evitare che il programma vada a cercare nel carattere successivo della lista (che non esiste)
+for i in range(len(ins_form)):
+    if ins_form[i].isupper() == True: #Controllo se la lettera e' maiuscola o minuscola per memorizzare l'elemento
+        if (len(ins_form) - 1) == i: #Se mi trovo a fine riga
+            elementi.append(ins_form[i]) #Inserisco l'elemento nella lista per evitare che il programma vada a cercare nel carattere successivo della lista (che non esiste)
             indici.append(1) #L'indice e' necessariamente 1
         else: #Non mi trovo a fine riga
-            if formula[i+1].isupper() == True: #Se anche quella dopo e' maiuscola la memorizzo nella lista
-                elementi.append(formula[i])
+            if ins_form[i+1].isupper() == True: #Se anche quella dopo e' maiuscola la memorizzo nella lista
+                elementi.append(ins_form[i])
                 indici.append(1) #Aggiungo l'indice "1" alla lista
-            elif formula[i+1].islower() == True:
-                elementi.append(formula[i] + formula[i + 1]) #Se la lettera dopo e' minuscola inserisco entrambe le lettere nella lista
+            elif ins_form[i+1].islower() == True:
+                elementi.append(ins_form[i] + ins_form[i + 1]) #Se la lettera dopo e' minuscola inserisco entrambe le lettere nella lista
                 #Controllo se dopo c'e' un indice
-                if (i + 1) == (len(formula) - 1): #Se questo era l'ultimo carattere
+                if (i + 1) == (len(ins_form) - 1): #Se questo era l'ultimo carattere
                     indici.append(1) #L'indice e' necessariamente 1
-                elif formula[i+2].isdigit() == True: #Controllo l'indice
-                    indici.append(int(formula[i+2]))
-                elif formula[i+2].isupper() == True: #Altrimenti la lettera dopo e' maiuscola e l'indice e' 1
+                elif ins_form[i+2].isdigit() == True: #Controllo l'indice
+                    indici.append(int(ins_form[i+2]))
+                elif ins_form[i+2].isupper() == True: #Altrimenti la lettera dopo e' maiuscola e l'indice e' 1
                     indici.append(1)
 
-            elif formula[i+1].isdigit() == True: #Se la lettera dopo e' una cifra (indice)
-                elementi.append(formula[i]) #Aggiungo il primo elemento alla lista
-                indici.append(int(formula[i+1])) #Aggiungo l'indice del primo elemento alla lista
+            elif ins_form[i+1].isdigit() == True: #Se la lettera dopo e' una cifra (indice)
+                elementi.append(ins_form[i]) #Aggiungo il primo elemento alla lista
+                indici.append(int(ins_form[i+1])) #Aggiungo l'indice del primo elemento alla lista
 
 
 
@@ -87,7 +87,7 @@ for i in range(len(elementi)):
         for z in range(len(elementi)):
             if elementi[z] != "O":
                 elem_2 = elementi[z]
-        #Formula IUPAC dell'Ossido (la lista prefissi e' sfalsata di 1 rispetto agli indici degli elementi)
+        #ìformula IUPAC dell'Ossido (la lista prefissi e' sfalsata di 1 rispetto agli indici degli elementi)
         nomeIupac = f"{prefissi[ind_oss - 1]}ossido di {prefissi[ind_2 - 1]}{dic[elem_2]}"
 
-print(f"Formula IUPAC: {nomeIupac}")
+print(f"formula IUPAC: {nomeIupac}")
